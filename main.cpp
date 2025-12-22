94aa06521aa1742e200e00f23edfc89d3abec8c4
Karl Keshavarzi <kkeshavarzi@outlook.com>
2025-12-21 19:01:31 -0800

    Working on updating magnetic field. About to implement curl

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 class Grid {
 private:
-    int Nx_, Ny_, Nz_;                          // Grid Size
+    std::size_t Nx_, Ny_, Nz_;                  // Grid Size
     double dx_, dy_, dz_;                       // Spatial Differentials
     double c_;                                  // Light Speed
     double dt_;                                 // Time Differential
@@ -13,7 +13,7 @@ private:
 
 public:
     // Constructor:
-    Grid( int new_Nx = 1, int new_Ny = 1, int new_Nz = 1,
+    Grid( std::size_t new_Nx = 1, std::size_t new_Ny = 1, std::size_t new_Nz = 1,
           double new_dx = 1.0, double new_dy = 1.0, double new_dz = 1.0,
           double new_c = 1.0 ):
     Nx_{ new_Nx }, Ny_{ new_Ny }, Nz_{ new_Nz },
@@ -29,7 +29,7 @@ public:
         By_ = std::make_unique<double[]>( grid_size );
         Bz_ = std::make_unique<double[]>( grid_size );
 
-        for ( int i{}; i < grid_size; ++i ) {
+        for ( std::size_t i{}; i < grid_size; ++i ) {
             Ex_[i] = 0.0;
             Ey_[i] = 0.0;
             Ez_[i] = 0.0;
@@ -41,7 +41,25 @@ public:
 
     // Solving:
     void update_B() {
+        // dB/dt = -curl( E )
+        // Stop at N - 1 to allow for boundary conditions.
+        for ( std::size_t k{}; k < Nz() - 1; ++k ) {
+            for ( std::size_t j{}; j < Ny() - 1; ++j ) {
+                for ( std::size_t i{}; i < Nx() - 1; ++i ) {
+                    // In X: B_x = B_x - dB_x = B_x - curl(E)_x
+                    // ∂Bx/∂t = -(∂Ez/∂y - ∂Ey/∂z)
+                    Bx_[idx(i,j,k)] -= 1;
 
+                    // In Y: B_y = B_y - dB_y = B_y - curl(E)_y
+                    // ∂By/∂t = -(∂Ex/∂z - ∂Ez/∂x)
+                    Bx_[idx(i,j,k)] -= 1;
+
+                    // In Z: B_z = B_z - dB_z = B_z - curl(E)_z
+                    // ∂Bz/∂t = -(∂Ey/∂x - ∂Ex/∂y)
+                    Bx_[idx(i,j,k)] -= 1;
+                }
+            }
+        }
     }
     void update_E() {
 
@@ -63,22 +81,19 @@ public:
     double dt() const {
         return dt_;
     }
-    int Nx() const {
+    std::size_t Nx() const {
         return Nx_;
     }
-    int Ny() const {
+    std::size_t Ny() const {
         return Ny_;
     }
-    int Nz() const {
+    std::size_t Nz() const {
         return Nz_;
     }
 
     // Setters:
 
     // Helpers:
-    double curl( double const dt ) {
-
-    }
     std::size_t idx( std::size_t const x, std::size_t const y, std::size_t const z ) const {
         return x + Nx() * ( y + Ny() * z );
     }
